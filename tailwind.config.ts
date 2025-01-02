df64907458a7b277a9117177127cc9a08dab9647
rythmn1111 <rythmnmagnani0@gmail.com>
2025-01-02 15:54:46 +0530

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'nintendo-ds': ['"Nintendo DS BIOS"', 'sans-serif']
+      }
     },
   },
   plugins: [],
