e71dca66e48cd2f90083dce4dfe0e9932b8794b7
rythmn1111 <rythmnmagnani0@gmail.com>
2025-01-05 16:08:02 +0530

    testing host

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import "wired-elements"
 
 export default function Home() {
   return <>
+    <wired-button>qw</wired-button>
+    <wired-button elevation="3">Elevation</wired-button>
   <h1>hello</h1>
   </>
 }
